327224043ca85bde3b040500e0ddb405c2c77101
nayaramatos <nayara.macedo.matos@gmail.com>
2020-08-13 11:48:29 -0300

    Iniciando backend

---
+++ AcheiDocumento/src/main/java/br/com/acheidocumento/modelo/Documento.java
@@ -0,0 +1,23 @@
+package br.com.acheidocumento.modelo;
+
+public class Documento {
+	private int iddocumento;
+	private String nome;
+
+	public int getIddocumento() {
+		return iddocumento;
+	}
+
+	public void setIddocumento(int iddocumento) {
+		this.iddocumento = iddocumento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
---
+++ AcheiDocumento/src/main/java/br/com/acheidocumento/modelo/Pessoa.java
@@ -0,0 +1,33 @@
+package br.com.acheidocumento.modelo;
+
+public class Pessoa {
+
+	private int idPessoa;
+	private String nome;
+	private String telefone;
+
+	public int getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(int idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
