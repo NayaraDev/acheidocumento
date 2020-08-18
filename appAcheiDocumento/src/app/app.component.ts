0cfe0a37e010e53b1976bf440ea7e39ab30c18cf
olciandro.magalhaes <olciandro.magalhaes@spread.com.br>
2020-08-18 09:07:01 -0300

    Ajustes na tela

--- appAcheiDocumento/src/app/app.component.ts
+++ appAcheiDocumento/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appAcheiDocumento';
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog){}
+
+  /**onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  <div class="container">
+  <div class="example-button-row">
+    <button mat-button color="primary">Achei</button>
+    <button mat-raised-button (click)="openDialog()" color="primary">Perdi</button>
+  </div>
+
+  <div mat-dialog-actions>
+    <button mat-button (click)="onNoClick()">No Thanks</button>
+    <button mat-button [mat-dialog-close]="data.animal" cdkFocusInitial>Ok</button>
+  </div>
+  
+</div>*/
 }
